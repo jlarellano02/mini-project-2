3eb9df44868553429506eea4b257ce709f4fa3c5
jlarellano02 <monjajenjenmon@gmail.com>
2024-01-17 14:53:15 +0800

    add menu items

--- menu_items.js
+++ menu_items.js
@@ -20,6 +20,56 @@ const menu = [
         description: "Stir Fried Lobsters With Your Choice Of Flavor (500 Grams Per Order)",
         price: 1649
     },
+    {
+        id: 3,
+        image: "/img/menu7.webp",
+        menu_name: "Shrimps",
+        description: "Stir Fried Shrimp With Your Choice Of Flavor (500 Grams Per Order)",
+        price: 749
+    },
+    {
+        id: 4,
+        image: "/img/menu8.webp",
+        menu_name: "Seashell Mix",
+        description: "Stir Fried Seashell Mix With Your Choice Of Flavor (500 Grams Per Order)",
+        price: 649
+    },
+    {
+        id: 5,
+        image: "/img/menu9.webp",
+        menu_name: "8 pcs. Mixed Wings",
+        description: "Fried Chicken Wings Coated In Our Signature Flavors Of Your Choice",
+        price: 329
+    },
+    {
+        id: 6,
+        image: "/img/menu10.webp",
+        menu_name: "8-pcs. Go Naked Wings",
+        description: "Fried Chicken Wings With Your Choice Of Flavors On The Side",
+        price: 329
+    },
+    {
+        id: 7,
+        image: "/img/menu11.webp",
+        menu_name: "Two-Piece Louisiana Fried Chicken",
+        description: "Two-Piece Louisiana Fried Chicken (With Rice & Drink)",
+        price: 239
+    },
+    {
+        id: 8,
+        image: "/img/menu12.webp",
+        menu_name: "6-Pcs. Louisiana Fried Chicken",
+        description: "",
+        price: 609
+    },
+    {
+        id: 9,
+        image: "/img/menu13.webp",
+        menu_name: "Fish Surprise & Fries",
+        description: "Deep Fried Tilapia Fillet & Fries With Tartar Dip",
+        price: 329
+    },
+    
 ];
 
 module.exports = { menu };
