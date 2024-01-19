41b8d6d91f738921229df1cd3cd9576e3e40a3c2
jlarellano02 <monjajenjenmon@gmail.com>
2024-01-19 15:33:44 +0800

    change cart.js

--- js/cart.js
+++ js/cart.js
@@ -3,13 +3,13 @@ async function getCartData(data) {
   let my_text = await my_obj.text();
   let cart = JSON.parse(my_text);
 
-  for (let i = 0; i < cart.length; i++) {
-    let row =
-      "<table class='table'><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Total Price</th></tr><tr>" + "<td>" + cart[i].menu_name +"</td><td>" + cart[i].price + "</td>"
-      "</tr></table>";
-
+  let row = "<table class='table'><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Total Price</th></tr>";
+  
+    for (let i = 0; i < cart.length; i++) {
+      row += "<tr><td>" + cart[i].menu_name + "</td><td>" + cart[i].price + "</td><td>" + cart[i].quantity + "</td><td>" + (cart[i].price * cart[i].quantity) + "</td></tr>";
+    }
+    row += "</table>";
     $("#cart-items").append(row);
   }
-}
 
 getCartData("http://localhost:3000/api/cart/");
