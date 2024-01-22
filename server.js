0175cc4dc54af3463a8758d63d23ff9f923959b6
jlarellano02 <monjajenjenmon@gmail.com>
2024-01-22 15:29:39 +0800

    added port

--- server.js
+++ server.js
@@ -79,5 +79,7 @@ app.get("/index", (req, res) => {
   });
 });
 
-app.listen(3000);
-console.log("Starting server...");
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
